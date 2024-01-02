e323b7ec5de7201f192c94d693573a1e2866c85d
Chuck Joshua Mbanaso <chuksjoshuaa@gmail.com>
2024-01-02 01:35:42 +0100

    WIP: Deploying to ethereum using hardhat

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,13 @@
+require("dotenv").config();
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
