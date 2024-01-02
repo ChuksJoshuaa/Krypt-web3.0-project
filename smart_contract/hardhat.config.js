1f687dec4ad65296157355139f9f8a04f85334be
Chuck Joshua Mbanaso <chuksjoshuaa@gmail.com>
2024-01-02 02:54:42 +0100

    fix deploy issue with smart contract

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -6,7 +6,7 @@ module.exports = {
   solidity: "0.8.19",
   networks: {
     sepolia: {
-      url: `https://eth-sepolia.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
       accounts: [process.env.PRIVATE_KEY],
     },
   },
--- smart_contract/scripts/deploy.js
+++ smart_contract/scripts/deploy.js
@@ -1,12 +1,10 @@
 const hre = require("hardhat");
 
 const main = async () => {
-  const Transactions = await hre.ethers.getContractFactory("Transactions");
-  const transactions = await Transactions.deploy();
+  const transactions = await hre.ethers.deployContract("Transactions");
+  await transactions.waitForDeployment();
 
-  await transactions.deployed();
-
-  console.log("Transactions deployed to:", transactions.address);
+  console.log("Transactions deployed to:", transactions.target);
 };
 
 const runMain = async () => {
